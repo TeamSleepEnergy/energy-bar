a6925c61fe3d8d98488342c79b92c9feab1c9b3a
Brian Ly <b6ly@ucsd.edu>
2018-11-23 14:25:52 -0800

    Remove old 'data' and 'health' pages, and fix code formatting

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     time(), setInterval(time, 1000);
 };
 
--- js/login.js
+++ js/login.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
     });
 });
 
-
 /**
  * Storing input from register-form to localStorage
  * 
