788037cc526cd3f245e1d7073229ee85cc36ce35
Nathan Chau <nychau@ucsd.edu>
2018-11-26 15:44:20 -0800

    changed energy display

--- js/energy-bar.js
+++ js/energy-bar.js
@@ -1,60 +1,51 @@
 /**
- * Will initialize the energy bar to an approximate energy percentage with 
+ * Will initialize the energy bar to an approximate energy percentage with
  * respect to the amount of time you intend to sleep.
  */
-function initBar() {
-    $("#bar").css("width", 100 + "%");
-    $("#bar").attr("aria-valuenow", "100");
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var dropDown = document.getElementById("UserInput");
-    var wakeUpString = dropDown.options[dropDown.selectedIndex].text;
-    var wakeHour = Number(wakeUpString.charAt(0) + wakeUpString.charAt(1));
-    var wakeMin = Number(wakeUpString.charAt(3) + wakeUpString.charAt(4));
-    var pmCheck = wakeUpString.charAt(6);
+ function initBar() {
+     //id.style.width = "100%";
+     //$("#bar").css("width", 100 + "%");
+     //$("#bar").attr("aria-valuenow", "100");
 
-    // Calculate hours slept to energy percentage
-    var sleptHours = 0;
-    if (pmCheck == "P") {
-        wakeHour += 12;
-    }
-    if (hour > wakeHour) {
-        sleptHours = 24 - hour + wakeHour;
+     /*$("#bar").css("width", 100 + "%");
+     $("#bar").attr("aria-valuenow", "100");*/
 
-    } else if (hour < wakeHour) {
-        sleptHours = wakeHour - hour;
-    }
+     var interval = setInterval(function () {
+       var date = new Date();
+       var hour = date.getHours();
+       var min = date.getMinutes();
+       var dropDown = document.getElementById("UserInput");
+       var wakeUpString = dropDown.options[dropDown.selectedIndex].text;
+       var wakeHour = Number(wakeUpString.charAt(0) + wakeUpString.charAt(1));
+       var wakeMin = Number(wakeUpString.charAt(3) + wakeUpString.charAt(4));
+       var pmCheck = wakeUpString.charAt(6);
+       var sleptHours = 0; /*Math.abs((24 - hour) + wakeHour);*/
+       if (pmCheck == "P" && hour != 12) {
+           wakeHour += 12;
+       }
+       if (hour > wakeHour) {
+           sleptHours = 24 - hour + wakeHour;
 
-    // Calculate minutes slept to energy percentage
-    var sleptMin = 0;
-    var carryover = 0;
-    if (min > wakeMin) {
-        carryover = 1;
-        sleptMin = Math.abs((60 - min) + wakeMin);
-    } else if (min == wakeMin) {
-        carryover = 0;
-        sleptMin = 0;
-    } else {
-        carryover = 0;
-        sleptMin = wakeMin - min;
-    }
+       } else if (hour < wakeHour) {
+           sleptHours = wakeHour - hour;
 
-    var totalSleep = (sleptHours - carryover) * 60 + sleptMin;
-    var current_progress = 100;
+       }
+       var sleptMin = Math.abs((60 - min) - wakeMin);
+       if (min > wakeMin) {
+           var carryover = 1;
+       } else {
+           var carryover = 0;
+       }
+       var totalSleep = (sleptHours - carryover) * 60 + sleptMin;
+       var current_progress = (totalSleep / 480) * 100;
 
-    if (totalSleep < 480) {
-        current_progress = (totalSleep / 480) * 100;
-    }
-
-    $("#bar").css("width", current_progress + "%")
-    var interval = setInterval(function () {
-        current_progress -= 1; //
-        $("#bar").css("width", current_progress + "%")
-            .attr("aria-valuenow", current_progress)
-        if (current_progress <= 0)
-            clearInterval(interval);
-    }, 288000); // Scale this to wake up time
-
+       /*$("#bar").css("width", current_progress + "%")
+         current_progress -= 1; //*/
+         $("#bar").css("width", current_progress + "%")
+             .attr("aria-valuenow", current_progress)
+         //.text(current_progress + "% Complete");
+         if (current_progress <= 0)
+             clearInterval(interval);
+     }, 300); //scale this to wake up time*/
     return current_progress;
 }
